1da3f08d481ab175e0bbcadf5a90de91952726ac
eareyes <eareyes@redborder.com>
2021-11-26 10:25:51 +0000

    first commit

--- resources/libraries/rbsocial_helpers.rb
+++ resources/libraries/rbsocial_helpers.rb
@@ -1,5 +1,27 @@
 module Rbsocial
   module Helpers
 
+    def config_hash(resource)
+      config = {}
+
+      #CONF SECTION
+      kafka_topic = resource["kafka_topic"]
+      log_level = resource["log_level"]
+      config["conf"] = {
+        "debug" => log_level,
+        "stdout" => 1,
+        "syslog" => 0,
+        "threads" => 1,
+        "timeout" => 40,
+        "max_snmp_fails" => 2,
+        "max_kafka_fails" => 2,
+        "sleep_main_thread" => 40,
+        "sleep_worker_thread" => 5,
+        "kafka_broker" => "kafka.service",
+        "kafka_timeout" => 2,
+        "kafka_topic" => kafka_topic
+      }
+      return config
+      end
   end
 end
--- resources/recipies/default.rb
+++ resources/recipies/default.rb
@@ -10,4 +10,8 @@ directory "etc/rb-social" do
   owner "root"
 end
 
-
+rbsocial_config "config" do
+  name node["hostname"]
+  log_level 7
+  action :add
+end
